3cdb6db8a4f81a141bd8b111350e099485411423
dodoreddy <79077997+dodoreddy@users.noreply.github.com>
2022-02-27 16:57:25 +0530

    Changes

--- file.py
+++ file.py
@@ -1,37 +1,41 @@
 import person
 
+
+# This is a class object for a line of people
 class queue:
+    # inits programs
     def __init__(self, line, y, start):
         self.line = line
         self.y = y
         self.start = start
 
+    # auto draws the queues
     def draw(self):
         line = self.line
+        # Starting x position
         x = self.start
+        # loops through the people in the queue
         for i in range(0, len(line)):
+            # changes x,y pos for the person
             line[i].move(x, self.y)
-            if i<8:
-
-                if x>0:
+            # displays only 8 people.
+            if i < 8:
+                # draws people
+                if x > 0:
                     line[i].draw()
-
-                if (i != len(line)-1):
+                # calulates the distance from other person
+                if (i != len(line) - 1):
                     x = x - (line[i].size + 5 + line[i + 1].size)
 
-
-
+    # method to add a random person to the queue
     def add(self, screen):
         self.line.append(person.person(0, 0, screen))
 
+    # method to add a specific person to queue
     def admit(self, screen, person):
         self.line.append(person)
 
+    # Removes first person form the queue
     def remove(self):
-        if len(self.line)>0:
+        if len(self.line) > 0:
             self.line.pop(0)
-
-
-
-    def average(self, x, y):
-        return (x+y)/2
